0a7540a45a047f1dccb05abcad1fff77a85fbebb
rnjsmj <rnjsmj0820@naver.com>
2024-02-22 17:14:32 +0900

    java11 Loop & applicate

--- test04/src/org/kh/app2/StarPrint.java
+++ test04/src/org/kh/app2/StarPrint.java
@@ -16,6 +16,49 @@ public class StarPrint {
 			}
 			System.out.println();
 		}
+		System.out.println();
+		
+		/*
+		*
+		**
+		***
+		****
+		*****
+		******
+		 */
+		
+		for (int i=1; i<7; i++) {
+			for (int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		/*
+		 ******
+		 *****
+		 ****
+		 ***
+		 **
+		 * 
+		 */
+		//1
+		for (int i=6; i>0; i--) {
+			for (int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		//2
+		for (int i=1; i<=6; i++) {
+			for (int j=0; j<7-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		
 		
 	}
